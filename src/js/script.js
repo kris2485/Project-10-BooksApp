6cf5666f485ea30c8b5901c53d9a567a902a71d5
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-03-14 13:42:33 +0100

    Add class BookList

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,41 @@
 {
   ('use strict');
+  const select = {
+    templateOf: {
+      templateBook: '#template-book',
+    },
+    containerOf: {
+      bookList: '.books-list',
+    },
+  };
+  const templates = {
+    templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
+  };
+  class BookList {
+    constructor() {
+      const thisBookList = this;
+      thisBookList.getElements();
+      thisBookList.render();
+    }
+
+    render() {
+      const thisBookList = this;
+      thisBookList.data = dataSource.books;
+      for (let book of thisBookList.data) {
+        const generatedHTML = templates.templateBook(book);
+        const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+        thisBookList.book.appendChild(generatedDOM);
+      }
+    }
+    getElements() {
+      const thisBookList = this;
+      thisBookList.book = document.querySelector(select.containerOf.bookList);
+    }
+  }
+  const app = {
+    initProject: function () {
+      new BookList();
+    },
+  };
+  app.initProject();
 }
